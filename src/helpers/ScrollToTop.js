ef5484071667e95d1b8b6a610a240b21f3f3f768
veekshan allada <veekshanallada@veekshans-MacBook-Pro-2.local>
2024-04-29 13:07:08 +0530

    browser's back overrides to hard reload

--- src/helpers/ScrollToTop.js
+++ src/helpers/ScrollToTop.js
@@ -3,8 +3,19 @@ import { withRouter } from "react-router-dom";
 
 const ScrollToTop = props => {
   useEffect(() => {
-    window.scrollTo(0, 0);
-  });
+    const unlisten = props.history.listen((location, action) => {
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+
+      if (action === "POP" || action === "PUSH") {
+        // Force a hard reload of the page
+        window.location.reload(true);
+      }
+    });
+    return () => {
+      unlisten();
+    };
+  }, [props.history]);
+
   return props.children;
 };
 
